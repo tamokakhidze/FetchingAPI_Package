14b16a19ccfbb428e305a4369235e386a1e1a188
Tamuna Kakhidze <tamuna.kakhidze.1@btu.edu.ge>
2024-05-01 18:01:15 +0400

    added file to sources

--- Package.swift
+++ Package.swift
@@ -7,14 +7,11 @@ import Foundation
 let package = Package(
     name: "NetworkServicePackage",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkServicePackage",
             targets: ["NetworkServicePackage"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NetworkServicePackage"),
         .testTarget(
@@ -23,46 +20,3 @@ let package = Package(
     ]
 )
 
-enum NetworkError: Error {
-    case decodeError
-    case wrongResponse
-    case wrongStatusCode(code: Int)
-}
-
-public class NetworkService {
-    
-    public init() {}
-    
-    public func getData<T: Decodable>(urlString: String, completion: @escaping (Result<T,Error>) ->(Void)) {
-        let url = URL(string: urlString)!
-        
-        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            
-            if let error {
-                print(error.localizedDescription)
-            }
-            
-            guard let response = response as? HTTPURLResponse else {
-                return
-            }
-            
-            guard (200...299).contains(response.statusCode) else {
-                print("Wrong status code: \(response.statusCode.description)")
-                return
-            }
-            
-            guard let data else { return }
-            
-            do {
-                let decoder = JSONDecoder()
-                let object = try decoder.decode(T.self, from: data)
-                
-                DispatchQueue.main.async {
-                    completion(.success(object))
-                }
-            } catch {
-                completion(.failure(NetworkError.decodeError))
-            }
-        }.resume()
-    }
-}
