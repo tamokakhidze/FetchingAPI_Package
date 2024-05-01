866e16a8510291f695f01916714347e034f17cb8
Tamuna Kakhidze <tamuna.kakhidze.1@btu.edu.ge>
2024-05-01 16:28:50 +0400

    first commit

---
+++ Package.swift
@@ -0,0 +1,65 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "NetworkServicePackage",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NetworkServicePackage",
+            targets: ["NetworkServicePackage"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NetworkServicePackage"),
+        .testTarget(
+            name: "NetworkServicePackageTests",
+            dependencies: ["NetworkServicePackage"]),
+    ]
+)
+
+enum NetworkError: Error {
+    case decodeError
+    case wrongResponse
+    case wrongStatusCode(code: Int)
+}
+
+class NetworkService {
+    func getData<T: Decodable>(urlString: String, completion: @escaping (Result<T,Error>) ->(Void)) {
+        let url = URL(string: urlString)!
+        
+        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            
+            if let error {
+                print(error.localizedDescription)
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                return
+            }
+            
+            guard (200...299).contains(response.statusCode) else {
+                print("Wrong status code: \(response.statusCode.description)")
+                return
+            }
+            
+            guard let data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let object = try decoder.decode(T.self, from: data)
+                
+                DispatchQueue.main.async {
+                    completion(.success(object))
+                }
+            } catch {
+                completion(.failure(NetworkError.decodeError))
+            }
+        }.resume()
+    }
+}
